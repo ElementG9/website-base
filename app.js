712e09450fff911f3d27b0ac2998d5dacd833012
ElementG9 <elementg9dev@gmail.com>
2018-10-10 16:40:40 -0600

    Added a 404 page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Config
 var port = 8080;
 var app = require("express")();
+var fs = require("fs");
 app.set("view engine", "pug");
 app.set('views', './views');
 
@@ -8,14 +9,24 @@ app.set('views', './views');
 app.get("/", (req, res) => {
     res.render("index");
 });
-app.get("/:file", (req, res) => {
-    res.render(req.params.file);
-});
 app.get("/style/:file", (req, res) => { // Serve CSS
     res.sendFile(`${__dirname}/public/style/${req.params.file}`);
 });
 app.get("/script/:file", (req, res) => { // Serve JS
     res.sendFile(`${__dirname}/public/scripts/${req.params.file}`);
+});
+app.get("/:file", (req, res) => {
+    var path = `${__dirname}/public/${req.params.file}`;
+    if (fs.existsSync(path)) { // if file exists
+        res.sendFile(path); // send file
+    } else {
+        res.status(404).render("404"); // else send 404
+    }
+});
+
+// 404 error
+app.use(function (req, res, next) {
+    res.status(404).render("404");
 });
 
 // Listen
