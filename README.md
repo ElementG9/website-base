c183ab289bc141e12656ab9041cc6a2fc91cfa5c
ElementG9 <elementg9dev@gmail.com>
2018-10-10 12:53:09 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # website-base
+## By ElementG9
+
+This repo is a project to speed up web development by providing the basic structure.
+
+Installation:
+* Run `npm i` to install the dependencies
+* Run `node app.js` to run the server
---
+++ app.js
@@ -0,0 +1,24 @@
+// Config
+var port = 8080;
+var app = require("express")();
+app.set("view engine", "pug");
+app.set('views', './views');
+
+// Routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
+app.get("/:file" (req, res) => {
+    res.render(req.params.file);
+});
+app.get("/style/:file", (req, res) => { // Serve CSS
+    res.sendFile(`${__dirname}/public/style/${req.params.file}`);
+});
+app.get("/script/:file", (req, res) => { // Serve JS
+    res.sendFile(`${__dirname}/public/scripts/${req.params.file}`);
+});
+
+// Listen
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
